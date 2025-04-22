cbf6c149833c5745e8e347f6dd3c09b78651168c
birros <birros.a.a.zefiryuz@gmail.com>
2025-04-22 09:07:10 +0700

    Adding Books CRUD

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,4 @@ Route::get('/user', function (Request $request) {
 
 
 Route::apiResource('users', UserController::class);
+Route::apiResource('books', BookController::class);
